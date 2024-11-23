8c0df645350ad4a3e7cd157b7db1607a1b6adbbf
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-24 00:05:57 +0500

    second

---
+++ boolean17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace boolean17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+            bool result = a>99 && a<1000 && a%2!=0;
+            WriteLine(result);
+        }
+    }
+}
